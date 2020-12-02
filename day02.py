8135f2b048d06742331810e8e0fa94a9d84d5b97
Sophie Kaelin <sophiekaelin@Sophies-MacBook-Pro.local>
2020-12-02 17:02:06 +1100

    cleanup day2

--- day02.py
+++ day02.py
@@ -1,33 +1,28 @@
 import re
 
-print("\n========== INPUTING FILE ==========")
-
-inFile = open("input/day02.txt")
+# Input File
 myList = []
-
+inFile = open("input/day02.txt")
 for line in inFile:
     myList.append(line.replace("\n",""))
 
-print("========== PART 1 ==========")
+# When a password is split: vals[0] = min, vals[1] = max, vals[2] = character, vals[4] = password
 
-# When a password is split:
-# vals[0] = min, vals[1] = max, vals[2] = character, vals[4] = password
-
-count1 = 0
-count2 = 0
+part1Count = 0
+part2Count = 0
 for pwd in myList :
     vals = re.split("-| |:", pwd)
     
     # PART 1
     numChars = vals[4].count(vals[2])
     if numChars >= int(vals[0]) and numChars <= int(vals[1]) :
-        count1 +=1
+        part1Count +=1
 
     # PART 2
     firstPos  = vals[4][int(vals[0]) - 1] == vals[2]
     secondPos = vals[4][int(vals[1]) - 1] == vals[2]
     if firstPos != secondPos :
-        count2 +=1
+        part2Count +=1
             
-print("PART1: There are: " + str(count1) + " corrupt passwords")
-print("PART2: There are: " + str(count2) + " invalid passwords")
+print("PART1: There are: " + str(part1Count) + " corrupt passwords")
+print("PART2: There are: " + str(part2Count) + " invalid passwords")
