3e9e32dcc75d7b25849d333ec37f990f46799c6f
Sophie Kaelin <sophiekaelin@Sophies-MacBook-Pro.local>
2020-12-03 17:18:43 +1100

    cleaned up day03

--- day03.py
+++ day03.py
@@ -1,7 +1,3 @@
-import re
-
-print("\n========== INPUTING FILE ==========")
-
 inFile = open("input/day03.txt")
 myList = []
 rows = 0
@@ -14,32 +10,10 @@ cols = len(myList[0])
 print("ROWS: " + str(rows))
 print("COLS: " + str(cols))
 
-print("========== PART 1 ==========")
+# combo = [column movement, row movement, totalTrees]
+combos = [[1, 1, 0], [3, 1, 0], [5, 1, 0], [7, 1, 0], [1, 2, 0]]
 
-curRow = 0
-curCol = 0
-numTrees = 0
-
-while (curRow != rows):
-    if(myList[curRow][curCol] == "#"):
-        numTrees += 1
-    curRow+=1
-    curCol = (curCol + 3) % cols
-
-
-print("You have hit " + str(numTrees) + " trees")
-
-print("========== PART 2 ==========")
-
-threeOne = numTrees
-oneOne = 0
-fiveOne = 0
-sevenOne = 0
-oneTwo = 0
-
-combos = [[1, 1, 0], [5, 1, 0], [7, 1, 0], [1, 2, 0]]
-
-for i in range(4):
+for i in range(len(combos)):
     curRow = 0
     curCol = 0
     numTrees = 0
@@ -50,5 +24,6 @@ for i in range(4):
         curRow+=combos[i][1]
         curCol = (curCol + combos[i][0]) % cols
     combos[i][2] = numTrees
+    print("You have hit " + str(combos[i][2]) + " trees")
 
-print("Multiplication is: " + str(combos[0][2] * combos[1][2] * combos[2][2] * combos[3][2] * threeOne))
+print("Multiplication is: " + str(combos[0][2] * combos[1][2] * combos[2][2] * combos[3][2] * combos[4][2]))
