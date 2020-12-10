8f96bc362fa3ac4842e8178deb24f5fa6d2a6d69
Sophie Kaelin <sophiekaelin@Sophies-MacBook-Pro.local>
2020-12-11 01:05:10 +1100

    Complete Day 9 part 1

--- day09.py
+++ day09.py
@@ -2,8 +2,22 @@ inFile = open("input/day09.txt")
 myList = []
 
 for line in inFile:
-    myList.append(line.replace("\n",""))
-myList.append("")
+    myList.append(int(line.replace("\n","")))
 
-print(myList)
 print("===== PART 1 =====")
+
+def addTwo(li, addsTo):
+    for x in range(len(li) - 1):
+        if(x > addsTo):
+            continue
+        for y in range(x+1, len(li)):
+            if li[x] + li[y] == addsTo:
+                return True
+    return False
+
+for i in range(25, len(myList)):
+    if not addTwo(myList[i-25:i], myList[i]):
+        print(myList[i])
+        break
+
+print("===== PART 2 =====")
