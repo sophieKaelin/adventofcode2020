a64d5d93099658a5eae1a7d47fbba303b79ebc22
Sophie Kaelin <sophiekaelin@Sophies-MacBook-Pro.local>
2020-12-11 00:25:08 +1100

    remove major duplication, still minor duplication

--- day07.py
+++ day07.py
@@ -7,23 +7,26 @@ for line in inFile:
     myList.append(line.replace("\n","").replace(".", "").replace(" bags", "").replace(" bag", ""))
 
 print("===== PART 1 =====")
-data = {}
+data1 = {}
+data2 = {}
 
 # Enter data into dictionary
 for entry in myList:
     values = re.split(" contain |, ", entry)
-    if values[1] != "no other bags":
-        data[values[0].replace(" bags", "")] = []
+    if values[1] != "no other":
+        data1[values[0].replace(" bags", "")] = []
+        data2[values[0].replace(" bags", "")] = []
         for x in range(1, len(values)):
-            data[values[0]].append(values[x][2:])
+            data1[values[0]].append(values[x][2:])
+            data2[values[0]].append([values[x][0], values[x][2:]])
 
 toCheck = ["shiny gold"]
 checked = []
 
 # Check through all the bags that contain silver gold or contain a bag that contains silver gold etc.
 while len(toCheck) > 0:
-    for item in data:
-        if toCheck[0] in data[item]:
+    for item in data1:
+        if toCheck[0] in data1[item]:
             if item not in checked:
                 toCheck.append(item)
                 checked.append(item)
@@ -32,16 +35,6 @@ while len(toCheck) > 0:
 print("Outer Bags: " + str(len(checked)))
 
 print("===== PART 2 =====")
-data = {}
-
-# Enter data into dictionary
-for entry in myList:
-    values = re.split(" contain |, ", entry)
-    if values[1] != "no other":
-        data[values[0].replace(" bags", "")] = []
-        for x in range(1, len(values)):
-            data[values[0]].append([values[x][0], values[x][2:]])
-# print(data)
 
 # DFS (sort of) starting from "shiny gold"
 def nextBag(toCheck, data):
@@ -53,4 +46,4 @@ def nextBag(toCheck, data):
             total += int(bags[0]) * nextBag(bags[1], data)
         return total
 
-print("The shiny gold bag contains " + str(nextBag("shiny gold", data)) + " bags (including itself)")
+print("The shiny gold bag contains " + str(nextBag("shiny gold", data2)) + " bags (including itself)")
