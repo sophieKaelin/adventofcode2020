9599f6e1a4af14445e4946688987b6e5e5ae5255
Sophie Kaelin <sophiekaelin@Sophies-MacBook-Pro.local>
2020-12-11 15:01:12 +1100

    day 10 part 2 (works for small inputs only)

--- day10.py
+++ day10.py
@@ -1,9 +1,38 @@
+import copy
 inFile = open("input/day10.txt")
-myList = []
+myList = [0]
 
 for line in inFile:
-    myList.append(line.replace("\n",""))
-myList.append("")
-
+    myList.append(int(line.replace("\n","")))
+myList.sort()
 print(myList)
+
 print("===== PART 1 =====")
+dif1, dif3 = 0, 1
+
+for i in range(len(myList)-1):
+    dif = myList[i+1] - myList[i]
+    # print( str(myList[i+1]) + " - " + str(myList[i]) + " == " + str(myList[i+1] - myList[i]))
+    if dif == 1:
+        dif1 += 1
+    else:
+        dif3 += 1
+
+print(dif1)
+print(dif3)
+print(dif1 * dif3)
+
+print("===== PART 2 =====")
+
+def part2(pos, li):
+    # print("li: " + str(li))
+    if len(li) < 3 or pos == len(li)-1:
+        return 1
+    if li[pos+1] - li[pos-1] <= 3: # Means you could keep or remove this item
+        temp = copy.deepcopy(li)
+        temp.pop(pos)
+        return part2(pos, temp) + part2(pos+1, li)
+    else:
+        return part2(pos+1, li)
+print(myList)
+print(part2(1, myList))
