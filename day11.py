e72c54a20034d4fbc9b852f91a89bc1fa2a4e4ef
Sophie Kaelin <sophiekaelin@Sophies-MacBook-Pro.local>
2020-12-13 15:17:15 +1100

    simplify day11 (still quite slow)

--- day11.py
+++ day11.py
@@ -1,3 +1,4 @@
+import copy
 inFile = open("input/day11.txt")
 myList = []
 
@@ -7,52 +8,24 @@ for line in inFile:
 maxRow = len(myList)
 maxCol = len(myList[0])
 
-print("~~ Part 1 ~~")
-# def countSurrounding(c, r, li):
-#     count = 0
-#     for ro in range(max(0,r-1), min(r+2, maxRow)):
-#         count += li[ro][max(0, c-1):min(maxCol, c+2)].count("#")
-#     if li[r][c] == "#":
-#         count -=1
-#     return count
+# Part 1 Functions
+def countSurrounding(c, r, li):
+    count = 0
+    for ro in range(max(0,r-1), min(r+2, maxRow)):
+        count += li[ro][max(0, c-1):min(maxCol, c+2)].count("#")
+    if li[r][c] == "#":
+        count -=1
+    return count
 
-# finished = False
-# while(not finished):
-#     finished = True
-#     newList = []
-#     for r in range(maxRow):
-#         s = ""
-#         for c in range(maxCol):
-#             if myList[r][c] != ".":
-#                 total = countSurrounding(c, r, myList)
-#                 if total == 0:
-#                     s += ("#")
-#                 elif total >= 4:
-#                     s += ("L")
-#                 else:
-#                     s += (myList[r][c])
-#                 finished = finished and s[c] == myList[r][c]
-#             else:
-#                 s += (".")
-#         newList.append(s)
-#     myList = newList
+# Part 2 Functions
+def inRange(c, r):
+    return c in range(0, maxCol) and r in range(0, maxRow)
 
-# # Count number of occupied seats
-# sum = 0
-# for v in myList:
-#     sum+= v.count("#")
-# print(sum)
-
-print("~~ Part 2 ~~")
-
-def inRange(tc, tr):
-    return tc in range(0, maxCol) and tr in range(0, maxRow)
-
-def checkDir(tc,tr, mvC, mvR, li):
-    while(inRange(tc, tr) and li[tr][tc] == "."):
-        tc += mvC
-        tr += mvR
-    if(inRange(tc, tr) and li[tr][tc] == "#"):
+def checkDir(c,r, mvC, mvR, li):
+    while(inRange(c, r) and li[r][c] == "."):
+        c += mvC
+        r += mvR
+    if(inRange(c, r) and li[r][c] == "#"):
         return 1
     return 0
 
@@ -68,30 +41,37 @@ def countVisuals(c, r, li):
     count += checkDir(c+1, r+1, 1, 1, li) #DOWN/RIGHT
     return count
 
-finished = False
-while(not finished):
-    finished = True
-    newList = []
-    for r in range(maxRow):
-        s = ""
-        for c in range(maxCol):
-            if myList[r][c] != ".":
-                total = countVisuals(c, r, myList)
-                if total == 0:
-                    s += ("#")
-                elif total >= 5:
-                    s += ("L")
+# Loop to change seat layout
+def day11Function (tolerance, f, myList):
+    finished = False
+    while(not finished):
+        finished = True
+        newList = []
+        for r in range(maxRow):
+            s = ""
+            for c in range(maxCol):
+                if myList[r][c] != ".":
+                    total = f(c, r, myList)
+                    if total == 0:
+                        s += ("#")
+                    elif total >= tolerance:
+                        s += ("L")
+                    else:
+                        s += (myList[r][c])
+                    finished = finished and s[c] == myList[r][c]
                 else:
-                    s += (myList[r][c])
-                finished = finished and s[c] == myList[r][c]
-            else:
-                s += (".")
-        newList.append(s)
-    myList = newList
+                    s += (".")
+            newList.append(s)
+        myList = newList
 
-# Count number of occupied seats
-sum = 0
-for v in myList:
-    sum+= v.count("#")
-print(sum)
+    # Count number of occupied seats
+    sum = 0
+    for v in myList:
+        sum+= v.count("#")
+    print(sum)
 
+print("~~ Part 1 ~~")
+day11Function(4, countSurrounding, copy.deepcopy(myList))
+
+print("~~ Part 2 ~~")
+day11Function(5, countVisuals, copy.deepcopy(myList))
